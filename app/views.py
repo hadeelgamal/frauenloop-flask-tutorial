6959dd3050ac93d58393c1ad15817f5bab9e7f05
Don Shine <dshine@gofree.indigo.ie>
2019-07-03 18:07:35 +0200

    render template

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 from app import app
 
 @app.route('/')
 def index():
-	return "<h1>Hello World!</h1>"
+	return render_template('index.html')
